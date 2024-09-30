7fed6ca253424b951e682ecda26ae9007c175066
Himadry Mondol <himadrymondol81@gmail.com>
2024-09-30 08:58:34 +0600

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,16 @@
+package newerajava;
+
+public class variables {
+
+	public static void main(String[] args) {
+		
+		int x; // decliaration 
+		x= 123; //innitialization 
+		// 
+		char symbol='a';
+		System.out.println("the number is " +x);
+		System.out.println(symbol);
+	}
+	}
+
+
