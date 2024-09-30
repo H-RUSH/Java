cd688c6b772d931bfd0fe361e285b5e3b4e3f817
Himadry Mondol <himadrymondol81@gmail.com>
2024-09-30 09:40:54 +0600

    Add files via upload

---
+++ swich.java
@@ -0,0 +1,23 @@
+package newerajava;
+
+public class swich {
+
+	public static void main(String[] args) {
+		
+		String x = "himadry";
+		String y= "anika";
+		String z= "russel" ;
+		String a= "nymphea";
+		String b;
+		b=x;
+		x=y;
+		y=z;
+		z=a;
+		a=b;
+		System.out.println("x:" +x);
+		System.out.println("y:" +y);
+		System.out.println("z:" +z);
+		System.out.println("a:" +a);
+	}
+
+}
